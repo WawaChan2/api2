902c4bd4e8d591b3f9c304be5e2e7aaced143702
WawaChan2 <wahhinchiwawa@gmail.com>
2026-06-22 12:17:03 +0800

    Quick renaming

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::inertia('/', 'Welcome')->name('home');
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
     Route::inertia('catalog', 'ProductCatalog')->name('catalog');
-    Route::inertia('order', 'OrderHistory')->name('order');
+    Route::inertia('orders', 'OrderHistory')->name('orders');
     Route::inertia('stats', 'UserStats')->name('stats');
     Route::inertia('cart', 'Cart')->name('cart');
 });
