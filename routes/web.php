dfaa9e96ab9f9b6499bee96aed558d91c1ff97b0
WawaChan2 <wahhinchiwawa@gmail.com>
2026-06-22 11:53:23 +0800

    Added some views

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::inertia('/', 'Welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
+    Route::inertia('catalog', 'ProductCatalog')->name('catalog');
+    Route::inertia('order', 'OrderHistory')->name('order');
+    Route::inertia('stats', 'UserStats')->name('stats');
+    Route::inertia('cart', 'Cart')->name('cart');
 });
 
 require __DIR__.'/settings.php';
