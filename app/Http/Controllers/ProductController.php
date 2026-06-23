6ca32b5318b743f56b58c70e81b7725013a0d258
WawaChan2 <wahhinchiwawa@gmail.com>
2026-06-23 11:25:48 +0800

    Get product with the appropriate category

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
     public function index()
     {
         return Inertia::render('ProductCatalog', [
-            'products' => Product::all(),
+            'products' => Product::with('category')->get(),
         ]);
     }
 
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::middleware(['auth', 'verified', 'role.admin'])->group(function () {
 });
 
 Route::middleware(['auth', 'verified', 'role.user'])->group(function () {
-    Route::inertia('catalog', 'ProductCatalog')->name('catalog');
+    Route::get('catalog', [ProductController::class, 'index'])->name('catalog');
     Route::get('orders', [OrderController::class, 'index'])->name('orders');
     Route::inertia('stats', 'UserStats')->name('stats');
     Route::inertia('cart', 'Cart')->name('cart');
